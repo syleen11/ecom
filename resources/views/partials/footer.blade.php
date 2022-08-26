d6e3734880583ee476f4414ab2c4e631097932d6
Ubuntu <farhan@project.fv5pygkh414e3psvfdudkq50db.zx.internal.cloudapp.net>
2022-08-26 16:20:48 +0000

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with">Made </i> by Syed Furqan</div>
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
